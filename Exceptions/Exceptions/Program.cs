5ce890dd03cb2b6df74ce6e695e576513f7e56d7
Андрей Боев <a.boev.it@gmail.com>
2022-11-02 16:08:15 +0300

    Added Exceptions

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static int SumSeq(int a, int b)
+        {
+            int sum = 0;
+            if (a > b)
+                throw new ArgumentsPositionException("Первый аргумент должен быть больше второго!");
+            for (int i = a; i <= b; i++)
+                sum += i;
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            // Программа не завершается аварийно!
+            try
+            {
+                // Исключительные ситуации
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                Console.WriteLine(SumSeq(a, b));
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Введено не число!");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Превышено допустимое значение!");
+            }
+            catch (ArgumentsPositionException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
